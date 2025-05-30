23dcdc71022e2e8481f14cac13b38e5bc07a3ced
marco <marcomaraglino5@gmail.com>
2025-05-30 15:22:54 +0200

    Fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import PostComponent from "./components/postComponent.jsx";
+import PostComponent from "./components/Post/postComponent.jsx";
 import LoginComponent from "./components/LoginComponent.jsx";
 import './app.css'
+import Navbar from "./components/Navbar/navbarComponent.jsx";
 import SignUpComponent from "./components/SignUpComponent.jsx";
 function App() {
 
   return (
     <>
-        <SignUpComponent></SignUpComponent>
+        <Navbar></Navbar>
+        <LoginComponent/>
+        <SignUpComponent/>
     </>
   )
 }
