dc8e39ad9ebdfec7d9e1836c090880dd9accf4e4
marco <marcomaraglino5@gmail.com>
2025-05-31 21:06:55 +0200

    Navbar modified

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,19 +1,29 @@
 import "./NavBar.css"
 import {Container, Navbar, Nav} from "react-bootstrap";
-import {Home, PlusSquare, User} from "lucide-react";
+import {Home, MessageCircle, PlusSquare, User} from "lucide-react";
 import {Outlet} from "react-router-dom";
 
 function NavBar() {
     return (
-        <Navbar className="navbar" collapseOnSelect sticky="top" variant="light">
-            <Container>
-                <Navbar.Brand className='nomeBrand' href="#home">Social Network</Navbar.Brand>
-                <Nav className="ms-auto gap-4">
-                    <Nav.Link href="#home"><Home/></Nav.Link>
-                    <Nav.Link href="#createpost"><PlusSquare/></Nav.Link>
-                    <Nav.Link href="#profile"><User/></Nav.Link>
+        <Navbar className="navbar p-3" collapseOnSelect variant="light">
+                <Nav className="align-items-center flex-column flex-fill">
+                    <Nav.Link className="nav-link" href="#home">
+                        <Home size={20} className="me-4"/>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link className="nav-link" href="#createpost">
+                        <PlusSquare size={20} className="me-4"/>
+                        Crea
+                    </Nav.Link>
+                    <Nav.Link className="nav-link" href="#chat">
+                        <MessageCircle size={20} className="me-4"/>
+                        Chat
+                    </Nav.Link>
+                    <Nav.Link className="nav-link" href="#profile">
+                    <User size={20} className="me-4"/>
+                    Profile
+                </Nav.Link>
                 </Nav>
-            </Container>
         </Navbar>
     )
 }
