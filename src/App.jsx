676f12aa44a8289a7bfcaa6741a52f7c1a4ad468
marco <marcomaraglino5@gmail.com>
2025-07-12 22:03:20 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Post from "./components/Post/Post.jsx";
 import Login from "./components/Login/Login.jsx";
 import Navbar from "./components/Navbar/NavBar.jsx";
 import SignUp from "./components/Login/SignUp.jsx";
-import './app.css'
+import './App.css'
 import {BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import Layout from "./views/Layout.jsx";
 import CreateEvent from "./views/CreateEvent/CreateEvent.jsx";
